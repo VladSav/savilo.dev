98a3a4e15ff63c830512f070ecd9fc44132fa85f
VladSav <vladislavsav95@gmail.com>
2021-02-28 00:51:15 +0300

    Some minor fixes

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,8 @@ import App from './App.vue'
 import router from './router'
 
 // import Swiper styles
-import 'swiper/swiper-bundle.css';
+// import 'swiper/swiper-bundle.css';
+// import 'swiper/components/pagination/pagination.css'
 
 const app = createApp(App)
 app.use(router)
